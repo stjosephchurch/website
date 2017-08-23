91f9c67b085058f7168b6fbb9a044f64354a0457
Riju Jacob <riju.jacob@primefocus.com>
2017-08-23 11:05:50 +0530

    Adding new app file for new layout

---
+++ church/resources/views/layout/app.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>St. Joseph Orthodox Syrian Church</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel="stylesheet" href="{{asset('css/main.css')}}">
+        <link rel="shortcut icon" href="{{asset('favicon.ico')}}">
+    </head>
+    <body class="index">
+        <div id="page-wrapper">
+            @include('layout._header')
+            @include('layout._banner')
+        </div>
+        <article id="main">
+            @include('section.notice')
+            @include('section.events')
+            @include('section.aboutus')
+            @include('section.message')
+
+        </article>
+        @include('section.footer')
+        @include('layout._scripts')
+    </body>
+</html>
